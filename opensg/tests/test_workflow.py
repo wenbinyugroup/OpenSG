59ce5e777be7b44aa253879f4e2bb0e0dc922be5
kbonney <kirkb1998@gmail.com>
2025-02-27 10:38:29 -0500

    update tests

--- opensg/tests/test_workflow.py
+++ opensg/tests/test_workflow.py
@@ -5,68 +5,65 @@ import opensg
 import filecmp
 
 
-example_data_path = ""
 testdir = dirname(abspath(str(__file__)))
 datadir = join(testdir, "data")
 
 class TestExample(unittest.TestCase):
-    
-    # def setup
-    # def close
     def setUp(self):
-        mesh_yaml = join(datadir, "bar_urc_shell_mesh.yaml")
-        mesh_data = opensg.load_yaml(mesh_yaml)
-
-        self.blade_mesh = opensg.BladeMesh(mesh_data)
         return super().setUp()
     
-    def test_segment_creation(self):
-        mesh_filename = "section.msh"
-        expected_mesh_filename = join(datadir, 'test_section.msh')
+    def test_workflow(self):
+        # Setup
+        mesh_yaml = join(datadir, "bar_urc_shell_mesh.yaml")
+        mesh_data = opensg.load_yaml(mesh_yaml)
+        blade_mesh = opensg.BladeMesh(mesh_data)
         
-        _ = self.blade_mesh.generate_segment_mesh(segment_index=1, filename=mesh_filename)
+        # Mesh
+        section_mesh = blade_mesh.generate_segment_mesh(segment_index=1, filename="section.msh")
+        assert filecmp.cmp("section.msh", "test_section.msh")
         
-        assert filecmp.cmp(mesh_filename, expected_mesh_filename)
-    
-    def test_ABD_matrix(self):
-        section_mesh = self.blade_mesh.generate_segment_mesh(segment_index=1, filename="section.msh")
+        # ABD
         abd = section_mesh.compute_ABD()
-        
         abd_concat = np.concat(abd)
-        # np.savetxt(join(datadir, 'abd_test.txt'), abd_concat) # use to reset ground truth
         
-        expected_abd = np.loadtxt(join(datadir, 'abd_test.txt'))
+        expected_abd = np.loadtxt(join(datadir, "test_abd.txt"))
         assert np.isclose(abd_concat, expected_abd).all()
+        
+        # timoshenko stiffness
+        seg_stiffness, lbound_stiffness = section_mesh.compute_timo_stiffness_segment(abd)
+        
+        expected_seg_stiffness = np.loadtxt(join(datadir, 'test_seg_stiffness.txt'))
+        expected_lbound_stiffness = np.loadtxt(join(datadir, 'test_lbound_stiffness.txt'))
+        assert np.isclose(seg_stiffness, expected_seg_stiffness).all()
+        assert np.isclose(lbound_stiffness, expected_lbound_stiffness).all()
+
+
+def run_workflow():
+    """This function regenerates the test results. Use this if updates to the code have
+    changed the expected outputs and these new outputs are what should be tested against.
+    """
+    # Setup
+    mesh_yaml = join(datadir, "bar_urc_shell_mesh.yaml")
+    mesh_data = opensg.load_yaml(mesh_yaml)
+    blade_mesh = opensg.BladeMesh(mesh_data)
     
-    def test_timo_stiffness(self):
-        pass
+    # Mesh
+    section_mesh = blade_mesh.generate_segment_mesh(segment_index=1, filename=join(datadir, "test_section.msh"))
     
-
-    def test_EB_stiffness(self):
-        # load expected values
-        # expected_ABD = np.loadtxt("")
-        # expected_stiffness = np.loadtxt("")
-        
-        section_mesh = self.blade_mesh.generate_segment_mesh(segment_index=1, filename="section.msh")
-        ABD = section_mesh.compute_ABD()
-        # assert np.isclsoe(ABD, expected_ABD).all()
-        
-        computed_stiffness_matrix = section_mesh.compute_stiffness_EB(ABD)
-        # np.savetxt(join(datadir, 'stiffness_test.txt'), computed_stiffness_matrix) # use to reset ground truth
-        
-        assert computed_stiffness_matrix.shape == (4,4)
-        
-        expected_stiffness_matrix = np.loadtxt(join(datadir, 'stiffness_test.txt'))
-        assert np.isclose(computed_stiffness_matrix, expected_stiffness_matrix).all()
-        
-
-
-"""
-[[ 5.6138e+09  7.8930e+07 -9.1787e+07 -6.2902e+08]
- [ 7.8930e+07  2.2724e+10 -6.0954e+08  2.0795e+08]
- [-9.1787e+07 -6.0954e+08  1.0064e+10  9.9959e+08]
- [-6.2902e+08  2.0795e+08  9.9959e+08  1.2617e+10]]
-"""
-
+    # ABD
+    abd = section_mesh.compute_ABD()
+    abd_concat = np.concat(abd)
+    
+    np.savetxt(join(datadir, 'test_abd.txt'), abd_concat) 
+    
+    # Timoshenko stiffness
+    seg_stiffness, lbound_stiffness = section_mesh.compute_timo_stiffness_segment(abd)
+    
+    np.savetxt(join(datadir, 'test_seg_stiffness.txt'), seg_stiffness)
+    np.savetxt(join(datadir, 'test_lbound_stiffness.txt'), lbound_stiffness)
+    
+    return
+    
 if __name__ == "__main__":
-    unittest.main()
+    # unittest.main()
+    run_workflow()
