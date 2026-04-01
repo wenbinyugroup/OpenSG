05bb7b9104f11ad9ed85873a2f90078f64574d09
kbonney <kirkb1998@gmail.com>
2026-04-01 11:18:57 -0400

    remove garbage files

--- setup.py
+++
@@ -1,9 +0,0 @@
-"""Setup script for OpenSG package.
-
-This file is maintained for backward compatibility.
-All configuration is now in pyproject.toml.
-"""
-
-from setuptools import setup
-
-setup() 
