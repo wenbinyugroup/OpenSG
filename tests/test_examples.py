c50ebe90365b812c596e07f64e776e730e661f08
kbonney <kirkb1998@gmail.com>
2026-04-02 13:42:44 -0400

    add test examples

---
+++ tests/test_examples.py
@@ -0,0 +1,36 @@
+import os
+import sys
+import unittest
+# import pytest
+from os import listdir
+from os.path import isfile, join
+from subprocess import call
+from tests import root_dir
+
+examples_dir = str(root_dir / "examples")
+
+
+class TestExamples(unittest.TestCase):
+    # @pytest.mark.time_consuming
+    def test_examples_run(self):
+        cwd = os.getcwd()
+        os.chdir(examples_dir)
+        example_files = [
+            f
+            for f in listdir(examples_dir)
+            if isfile(join(examples_dir, f)) and f.endswith(".py")
+        ]
+        failed = []
+        for f in example_files:
+            rc = call([sys.executable, join(examples_dir, f)])
+            print(f, rc)
+            if rc != 0:
+                failed.append(f)
+        os.chdir(cwd)
+        if failed:
+            print("Failed examples: {0}".format(failed))
+        self.assertEqual(len(failed), 0)
+
+
+if __name__ == "__main__":
+    unittest.main()
